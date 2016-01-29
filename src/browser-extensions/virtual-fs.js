c616a47d2554962bcedec7d8aeb10af7aca35eec
Benoit Benezech <benoit.benezech@gmail.com>
2016-01-29 14:50:42 +0100

    fix filesystem

--- src/browser-extensions/virtual-fs.js
+++ src/browser-extensions/virtual-fs.js
@@ -3,20 +3,36 @@
 
 // var b64 = require('./base64.js').base64DecToArr;
 function VirtualFileSystem() {
-	this.fileSystem = {};
-	this.baseSystem = {};
+  this.fileSystem = {};
+  this.baseSystem = {};
 }
 
 VirtualFileSystem.prototype.readFileSync = function(filename) {
-	return this.baseSystem[filename] || this.fileSystem[filename];
+  console.log('reading', filename, '->', fixFilename(filename), this.baseSystem, this.fileSystem);
+  filename = fixFilename(filename);
+
+  return this.baseSystem[filename] || this.fileSystem[filename];
 };
 
 VirtualFileSystem.prototype.writeFileSync = function(filename, content) {
-	this.fileSystem[fixFilename(filename)] = content;
+  this.fileSystem[fixFilename(filename)] = content;
 };
 
 VirtualFileSystem.prototype.bindFS = function(data) {
-	this.baseSystem = data;
+  this.baseSystem = data;
 };
+
+
+function fixFilename(filename) {
+  if (filename.indexOf(__dirname) === 0) {
+    filename = filename.substring(__dirname.length);
+  }
+
+  if (filename.indexOf('/') === 0) {
+    filename = filename.substring(1);
+  }
+
+  return filename;
+}
 
 module.exports = new VirtualFileSystem();
