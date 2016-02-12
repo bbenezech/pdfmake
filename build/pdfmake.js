76a933ce4a9f351623da9a66af44c3ea17d6178b
Benoit Benezech <benoit.benezech@gmail.com>
2016-02-12 22:05:54 +0100

    Trying https://github.com/bpampuch/pdfmake/pull/475
    for IE11

--- build/pdfmake.js
+++ build/pdfmake.js
@@ -76,27 +76,27 @@
 	};
 
 	function Document(docDefinition, fonts, vfs) {
-		this.docDefinition = docDefinition;
-		this.fonts = fonts || defaultClientFonts;
-		this.vfs = vfs;
+	  this.docDefinition = docDefinition;
+	  this.fonts = fonts || defaultClientFonts;
+	  this.vfs = vfs;
 	}
 
 	Document.prototype._createDoc = function(options, callback) {
-		var printer = new PdfPrinter(this.fonts);
-		printer.fs.bindFS(this.vfs);
+	  var printer = new PdfPrinter(this.fonts);
+	  printer.fs.bindFS(this.vfs);
 
-		var doc = printer.createPdfKitDocument(this.docDefinition, options);
-		var chunks = [];
-		var result;
+	  var doc = printer.createPdfKitDocument(this.docDefinition, options);
+	  var chunks = [];
+	  var result;
 
-		doc.on('data', function(chunk) {
-			chunks.push(chunk);
-		});
-		doc.on('end', function() {
-			result = Buffer.concat(chunks);
-			callback(result, doc._pdfMakePages);
-		});
-		doc.end();
+	  doc.on('data', function(chunk) {
+	    chunks.push(chunk);
+	  });
+	  doc.on('end', function() {
+	    result = Buffer.concat(chunks);
+	    callback(result, doc._pdfMakePages);
+	  });
+	  doc.end();
 	};
 
 	Document.prototype._getPages = function(options, cb){
@@ -107,37 +107,71 @@
 	};
 
 	Document.prototype.open = function(message) {
-		// we have to open the window immediately and store the reference
-		// otherwise popup blockers will stop us
-		var win = window.open('', '_blank');
-
-		try {
-			this.getDataUrl(function(result) {
-				win.location.href = result;
-			});
-		} catch(e) {
-			win.close();
-			throw e;
-		}
+	  // we have to open the window immediately and store the reference
+	  // otherwise popup blockers will stop us
+	  var win = window.open('', '_blank');
+	  
+	  try {
+	    this.getBuffer(function (result) {
+	      var blob;
+	      try {
+	        blob = new Blob([result], { type: 'application/pdf' });
+	      } catch (e) {
+	        // Old browser which can't handle it without making it an byte array (ie10) 
+	        if (e.name == "InvalidStateError") {
+	          var byteArray = new Uint8Array(result);
+	          blob = new Blob([byteArray.buffer], { type: 'application/pdf' });
+	        }
+	      }
+	      
+	      if (blob) {
+	        var urlCreator = window.URL || window.webkitURL;
+	        var pdfUrl = urlCreator.createObjectURL( blob );
+	        win.location.href = pdfUrl;
+	      } else {
+	        throw 'Could not generate blob';
+	      }
+	    },  { autoPrint: false });
+	  } catch(e) {
+	    win.close();
+	    throw e;
+	  }
 	};
 
 
 	Document.prototype.print = function() {
-	  this.getDataUrl(function(dataUrl) {
-	    var iFrame = document.createElement('iframe');
-	    iFrame.style.position = 'absolute';
-	    iFrame.style.left = '-99999px';
-	    iFrame.src = dataUrl;
-	    iFrame.onload = function() {
-	      function removeIFrame(){
-	        document.body.removeChild(iFrame);
-	        document.removeEventListener('click', removeIFrame);
+	  this.getBuffer(function (result) {
+	    var blob;
+	    try {
+	      blob = new Blob([result], { type: 'application/pdf' });
+	    } catch (e) {
+	      // Old browser which can't handle it without making it an byte array (ie10) 
+	      if (e.name == "InvalidStateError") {
+	        var byteArray = new Uint8Array(result);
+	        blob = new Blob([byteArray.buffer], { type: 'application/pdf' });
 	      }
-	      document.addEventListener('click', removeIFrame, false);
-	    };
-
-	    document.body.appendChild(iFrame);
-	  }, { autoPrint: true });
+	    }
+	    
+	    if (blob) {
+	      var urlCreator = window.URL || window.webkitURL;
+	      var pdfUrl = urlCreator.createObjectURL( blob );
+	      var iFrame = document.createElement('iframe');
+	      iFrame.style.position = 'absolute';
+	      iFrame.style.left = '-99999px';
+	      iFrame.src = pdfUrl;
+	      iFrame.onload = function() {
+	        function removeIFrame(){
+	          document.body.removeChild(iFrame);
+	          document.removeEventListener('click', removeIFrame);
+	        }
+	        document.addEventListener('click', removeIFrame, false);
+	      };
+	      
+	      document.body.appendChild(iFrame);   
+	    } else {
+	      throw 'Could not generate blob';
+	    }
+	  },  { autoPrint: true });
 	};
 
 	Document.prototype.download = function(defaultFileName, cb) {
@@ -172,30 +206,30 @@
 	};
 
 	Document.prototype.getBase64 = function(cb, options) {
-		if (!cb) throw 'getBase64 is an async method and needs a callback argument';
-		this._createDoc(options, function(buffer) {
-			cb(buffer.toString('base64'));
-		});
+	  if (!cb) throw 'getBase64 is an async method and needs a callback argument';
+	  this._createDoc(options, function(buffer) {
+	    cb(buffer.toString('base64'));
+	  });
 	};
 
 	Document.prototype.getDataUrl = function(cb, options) {
-		if (!cb) throw 'getDataUrl is an async method and needs a callback argument';
-		this._createDoc(options, function(buffer) {
-			cb('data:application/pdf;base64,' + buffer.toString('base64'));
-		});
+	  if (!cb) throw 'getDataUrl is an async method and needs a callback argument';
+	  this._createDoc(options, function(buffer) {
+	    cb('data:application/pdf;base64,' + buffer.toString('base64'));
+	  });
 	};
 
 	Document.prototype.getBuffer = function(cb, options) {
-		if (!cb) throw 'getBuffer is an async method and needs a callback argument';
-		this._createDoc(options, function(buffer){
+	  if (!cb) throw 'getBuffer is an async method and needs a callback argument';
+	  this._createDoc(options, function(buffer){
 	    cb(buffer);
 	  });
 	};
 
 	module.exports = {
-		createPdf: function(docDefinition) {
-			return new Document(docDefinition, window.pdfMake.fonts, window.pdfMake.vfs);
-		}
+	  createPdf: function(docDefinition) {
+	    return new Document(docDefinition, window.pdfMake.fonts, window.pdfMake.vfs);
+	  }
 	};
 
 	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(2).Buffer))
